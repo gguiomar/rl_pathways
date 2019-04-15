c8c703c82e290060f2cace2d959eb62112825fa1
gguiomar <goncalo.guiomar@gmail.com>
2019-04-15 21:40:00 +0100

    First Commit

---
+++ agents/pathway_agents.py
@@ -0,0 +1,289 @@
+# RL agents with pathway separation
+import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+import matplotlib.gridspec as gridspec
+import copy as copy
+import sys
+import os
+
+class pathway_agents():
+    
+    # things to implement:
+    # 1. automatic splitting of trial identities (left,right) for any number of second tones
+    
+    def __init__(self, n_episodes, beta, gamma, rho, tsteps, second_tone_list, 
+    rwd_mag, pun_mag, print_flag, policy, alpha_i, save_history, learning_rule):
+
+        # agent variables
+        self.rwd_prob = 1   
+        self.pun_prob = 1
+        self.rwd_mag = rwd_mag
+        self.pun_mag = pun_mag
+        self.rho = rho
+        self.n_episodes = n_episodes
+        self.beta = beta
+        self.gamma = gamma
+        self.tsteps = tsteps
+        self.second_tone_list = second_tone_list
+        self.print_flag = print_flag
+        self.policy = policy
+        self.alpha_i = alpha_i
+        self.save_history = save_history 
+        self.learning_rule = learning_rule
+        self.n_actions = 3
+        self.n_pathways = 2
+        self.n_test_episodes = 100
+
+        # importing the environment
+        self.get_environment()
+
+    def get_environment(self):
+
+        # this function calls the timing task classical conditioning environment only
+
+        cwd = os.getcwd()
+        # changing the initial path given the current machine
+        usr_path = ''
+        for i in range(3):
+            usr_path += str(cwd.split("/")[i]) + str('/')
+
+        env_path = usr_path + 'Dropbox/phd/code/d1d2_project/classical_conditioning/'
+        sys.path.append(env_path)
+
+        from environments.timing_task_csc import timing_task_csc
+
+        self.env = timing_task_csc(self.tsteps, self.second_tone_list, self.rwd_mag, self.rwd_prob, self.pun_mag, self.pun_prob)
+
+    #  transfer functions for the td error ---
+    def tdp(self, x):
+        if x > 0:
+            return x
+        else:
+            return 0
+
+    def tdn(self, x):
+        if x < 0:
+            return -x
+        else:
+            return 0
+
+    def hev(self, v):
+        return np.heaviside(v, 0)
+
+    def normalise_vec(self, vec):
+        return (vec-min(vec))/(max(vec)-min(vec))
+
+    def nl_tdp(self, x):
+        sc = 6
+        return sc*np.tanh(x)+sc*0.9
+
+    def nl_tdn(self, x):
+        sc = 5
+        return sc*np.tanh(-x)+sc*0.5
+
+    # policies ---
+    def e_greedy(self, V, epsilon):
+
+        # e-greedy
+        if np.random.uniform(0, 1) > epsilon:
+            action = np.random.choice(len(V))
+        else:
+            action = np.argmax(V)
+
+        return int(action)
+
+    def softmax(self, A, beta):
+        # Q can be either the state value function or the
+        # advantage function
+
+        # big betas are lower temperatures -> more greedy
+
+        e_x = np.exp(beta * A)
+        action_probability = e_x / e_x.sum()
+
+        # multinomial sampling
+        selected_action = np.argmax(
+            np.random.multinomial(1, action_probability, size=1))
+
+        return selected_action
+
+    # tranining algorithm
+    def train_agent(self):
+
+        current_state = 0
+        next_state = 0
+        reward = 0
+        action = 0
+
+        # renaming environment
+        env = self.env
+
+        # learning rates for actor and critic - smaler alphas -> more iterations to converge
+        alpha_v_cap = 0.01
+        alpha_a_cap = 0.001
+        alpha_v = np.ones(env.n_total_states)
+        alpha_a = np.ones(env.n_total_states)
+
+        # convergence measures
+        pV = np.zeros(env.n_total_states)
+        pA = np.zeros((self.n_pathways, env.n_total_states, self.n_actions))
+        diffV = np.zeros(self.n_episodes)
+        diffA = np.zeros((self.n_episodes, self.n_pathways, self.n_actions))
+
+        # pathway weights - Anne Collins B_G and B_N respectively
+        w_D = 1 + self.rho
+        w_I = 1 - self.rho
+
+        # td-error
+        delta = 0
+
+        # trial variables
+        trial_length = []
+        state_visits = np.zeros(env.n_total_states)
+
+        # value functions for the simulations
+        V = np.zeros(env.n_total_states)  # state value function
+        Act = np.zeros((env.n_total_states, self.n_actions)) # action value function
+        A = np.random.uniform(0, 0.01, (self.n_pathways, env.n_total_states, self.n_actions))  # action weights
+
+        # generating variables to record trial history
+        if self.save_history:
+            test_data = np.zeros((self.n_episodes, env.n_states, 6))
+            V_h = np.zeros((self.n_episodes, env.n_total_states))
+            A_h = np.zeros((self.n_episodes, self.n_pathways, env.n_total_states, self.n_actions))
+            delta_h = np.zeros((self.n_episodes, len(self.second_tone_list), self.tsteps))
+            trial_seq = np.zeros((self.n_episodes, len(self.second_tone_list)))
+
+        choices = np.zeros(self.n_test_episodes)
+
+
+        for episode in range(self.n_episodes):
+
+            trial_type = np.random.choice(np.arange(len(self.second_tone_list)))
+            if self.save_history:
+                trial_seq[episode, trial_type] = 1
+
+            x = env.trials[trial_type, :, :]
+
+            # comparing value functions
+            pV = copy.copy(V)
+            pA = copy.copy(A)
+
+            current_state = 0
+
+            for t in range(env.n_states):
+
+                if type(self.policy) == int:
+                    # action selection ------
+                    if self.policy == 0:
+                        action = env.opt_act[trial_type, current_state].astype(int)
+                    elif self.policy == 1:
+                        action = self.softmax(Act[current_state, :], self.beta)
+                else:  # semi-optimal policy
+                    action = self.policy[t].astype(int)
+
+                # state update ------
+                next_state, reward = env.get_outcome(trial_type, current_state, action)
+
+                # td update // reward has to be of the next_state ------
+                delta = reward + self.gamma * V[next_state] - V[current_state]
+                if self.save_history:
+                    delta_h[episode, trial_type, t] = delta
+
+                # calculating alpha -------
+                state_visits[current_state] += 1
+                inv_sv = self.alpha_i / state_visits[current_state]
+
+                if inv_sv < alpha_v_cap:
+                    alpha_v[current_state] = inv_sv
+                else:
+                    alpha_v[current_state] = alpha_v_cap
+
+                if inv_sv < alpha_a_cap:
+                    alpha_a[current_state] = inv_sv
+                else:
+                    alpha_a[current_state] = alpha_a_cap
+
+                # update value functions ------ ANNE COLLINS & TRANSFER FUNCTION
+                if self.learning_rule == 'anne_collins':
+                    if current_state < env.n_total_states - 5:
+                        # state value function updating
+                        V[current_state] += alpha_v[current_state] * delta
+
+                        # actor weights
+                        A[0, current_state, action] += alpha_a[current_state] * A[0, current_state, action] * delta  # direct pathway
+                        A[1, current_state, action] += alpha_a[current_state] * A[1, current_state, action] * (-delta)  # indirect pathway
+
+                        # constrain actor values to be positive - heaviside function
+                        A[0, current_state, action] = self.tdp(A[0, current_state, action])
+                        A[1, current_state, action] = self.tdp(A[1, current_state, action])
+
+                        # calculate actor values
+                        Act[current_state, action] = w_D * A[0, current_state, action] - w_I * A[1, current_state, action]
+
+                    else:  # clamp the final states to zero (terminal states)
+                        V[current_state] = 0
+                        A[0, current_state, action] = 0
+                        A[1, current_state, action] = 0
+                        Act[current_state, action] = 0
+
+
+                if self.learning_rule == 'transfer_function':
+                    if current_state < env.n_total_states - 5:
+                        # state value function updating
+                        V[current_state] += alpha_v[current_state] * delta
+
+                        # advantage updating - mark humphries transfer function
+                        A[0, current_state, action] += alpha_a[current_state] * (self.nl_tdp(delta) - A[0, current_state, action])  # direct pathway
+                        A[1, current_state, action] += alpha_a[current_state] * (self.nl_tdn(delta) - A[1, current_state, action])  # indirect pathway
+
+                        # calculate actor values
+                        Act[current_state, action] = w_D * A[0, current_state, action] - w_I * A[1, current_state, action]
+
+                    else:  # clamp the final states to zero (terminal states)
+                        V[current_state] = 0
+                        A[0, current_state, action] = 0
+                        A[1, current_state, action] = 0
+                        Act[current_state, action] = 0
+
+                # saving relevant variables ------
+                if self.save_history:
+                    V_h[episode, :] = V
+                    A_h[episode, :] = A
+                    test_data[episode, t, :] = np.asarray([trial_type, current_state, action, next_state, reward, delta])
+
+                if self.print_flag and episode < 100:
+                    print([trial_type, current_state, action, next_state, reward, delta])
+
+                # current state update
+                current_state = next_state
+
+                if current_state == env.n_total_states-5:
+
+                    # calculating differences
+                    diffV[episode] = np.sum((V-pV)**2)
+                    for p in range(self.n_pathways):
+                        for a in range(self.n_actions):
+                            diffA[episode, p, a] = np.sum((A[p, :, a] - pA[p, :, a])**2)
+
+                    # performance related variables
+                    if episode > self.n_episodes - self.n_test_episodes:
+                        if reward <= 0:
+                            choices[self.n_episodes - episode - self.n_test_episodes] = 0
+                        if reward > 0:
+                            choices[self.n_episodes - episode - self.n_test_episodes] = 1
+ 
+                    break
+
+        if self.save_history:
+            data_dict_hist = {'V': V, 'state_visits': state_visits, 'A': A, 'Act': Act,
+                            'diffV': diffV, 'diffA': diffA, 'env': env, 'choices': choices,
+                            'test_data': test_data, 'diffV': diffV, 'diffA': diffA, 'trial_seq': trial_seq,
+                            'choices': choices, 'V_h': V_h, 'A_h': A_h, 'delta_h': delta_h}
+            return data_dict_hist
+        else:
+            data_dict_no_hist = {'V': V, 'state_visits': state_visits, 'A': A, 'Act': Act,
+                                'diffV': diffV, 'diffA': diffA, 'choices': choices}
+            return data_dict_no_hist
---
+++ environments/timing_task_csc.py
@@ -0,0 +1,200 @@
+import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+import matplotlib.gridspec as gridspec
+import copy as copy
+
+class timing_task_csc():
+    
+    # things to implement:
+    # 1. automatic splitting of trial identities (left,right) for any number of second tones
+    
+    def __init__(self, n_states, second_tone_list, reward_magnitude, 
+                 reward_probability, punishment_magnitude, punishment_probability):
+        
+        # Task variables
+        self.n_states = n_states # separate total number of transitions from total number of states
+        self.n_total_states = 2 * n_states + 5
+        self.n_actions = 3
+        
+        self.action_dict = np.arange(self.n_actions)
+        self.second_tone_list = second_tone_list
+        self.trial_types = len(self.second_tone_list)
+        self.n_trials = len(self.second_tone_list)
+        self.trials = np.zeros((self.n_trials, self.n_states*2, self.n_states))
+
+        
+        # Reward variables
+        self.reward_state = [0,0]
+        self.reward_magnitude = reward_magnitude
+        self.punishment_magnitude = punishment_magnitude
+        self.reward_probability = reward_probability
+        self.punishment_probability = punishment_probability
+
+        self.generate_environment()
+
+
+    def generate_environment(self):
+        self.generate_state_representation()
+        self.generate_state_sequence()
+        self.generate_environment_solution(0)
+    
+    def generate_state_representation(self):
+        
+        self.n_trials = len(self.second_tone_list)
+        tone1 = np.zeros((self.n_trials, self.n_states, self.n_states))
+        tone2 = np.zeros((self.n_trials, self.n_states, self.n_states))
+        self.trials = np.zeros((self.n_trials, self.n_states*2, self.n_states))
+
+        for k,e in enumerate(self.second_tone_list):
+            for i in range(self.n_states):
+                if i > self.n_states-5:
+                    tone1[k,i,i] = 0
+                    tone2[k,i,i] = 1
+                elif i < e:
+                    tone1[k,i,i] = 1
+                    tone2[k,i,i] = 0
+                else:
+                    tone1[k,i,i] = 0
+                    tone2[k,i,i] = 1
+                    
+            #self.trials.append(np.concatenate((tone1[k,:,:], tone2[k,:,:]), axis = 0))
+            self.trials[k,0:self.n_states,:] = tone1[k,:,:]
+            self.trials[k,self.n_states: 2*self.n_states,:] = tone2[k,:,:]
+        
+    def generate_state_sequence(self):
+        
+        # this function generates the state identities for the MDP
+        # that will be used in the simulation
+        
+        state = 0
+        self.trial_st = np.zeros((self.n_trials, self.n_states))
+        for k in range(self.n_trials):
+            cnt = 0
+            base = 0
+            for i,e in enumerate(self.trials[k].T):
+                state = np.nonzero(e)[0]
+                if i == 0:
+                    self.trial_st[k,i] = 0
+                if i != 0 and state != 0:
+                    self.trial_st[k,i] = state
+                if not state and i != 0:
+                    self.trial_st[k,i] = base + cnt
+                    cnt += 1
+        
+        self.trial_st = self.trial_st.astype(int)
+       
+    def plot_state_representation(self):
+        
+        fig = plt.figure(figsize=(15,10))
+        gs = gridspec.GridSpec(1, len(self.second_tone_list))
+        ax = []
+
+        #titles = ['short 1', 'short 2', 'long 1', 'long 2']
+        for k,e in enumerate(self.second_tone_list):
+            ax.append([])
+            ax[k] = fig.add_subplot(gs[0,k])
+            if k == 0: 
+                ax[k].set_ylabel('State')
+            ax[k].set_xlabel('Time')
+            ax[k].imshow(self.trials[k], cmap='Greys')
+            #ax[k].set_title(titles[k])
+            
+        plt.show()
+    
+    
+    def generate_environment_solution(self, plot_flag):
+        
+        # generates the policy for the optimal agent
+        
+        # map second tone to state identity
+        self.second_tone_state = np.zeros(self.n_trials)
+        for tr,st in enumerate(self.second_tone_list):
+            self.second_tone_state[tr] = np.nonzero(self.trials[tr][:,st])[0][0]
+
+        # map state identity to optimal action
+        self.opt_act = 2 * np.ones((self.n_trials, self.n_total_states))
+        for tr in range(self.n_trials):
+            for i,state in enumerate(self.trial_st[tr]):
+                if state >= self.second_tone_state[tr]:
+                    # split decisions in the middle
+                    if tr < self.n_trials/2: # short decision
+                        self.opt_act[tr,state] = 0
+                    else: # long decision
+                        self.opt_act[tr,state] = 1
+     
+        # take into account that the opt_act vector will have a set of 2's
+        # in the end because of the 0 states after the episode finishes
+        
+        if plot_flag:
+            plt.figure(figsize =(20,10))
+            plt.title('Optimal actions')
+            plt.imshow(self.opt_act)
+            plt.colorbar(fraction = 0.01)
+            plt.ylabel('trial type')
+            plt.xlabel('state')
+            plt.show()
+            
+    def test_environment_all(self):
+        
+        for tt in range(self.trial_types):
+            for cs in self.trial_st[tt]:
+                for a in range(self.n_actions):
+                    print([tt, cs, a], self.get_outcome(tt, cs, a))
+                    
+    def test_environment_action(self, action):
+        current_state = 0
+        next_state = 0
+        for i,tr in enumerate(second_tone_list):
+            for j,current_state in enumerate(env.trial_st[i]):
+                next_state, reward = env.get_outcome(i, current_state, action)
+                print([i, current_state, action, next_state, reward])
+    
+    def get_outcome(self, current_trial, current_state, action):
+        
+        next_state = 0
+        reward = 0
+        check_valid_state = np.argwhere(self.trial_st[current_trial] == current_state).shape[0] # should be > 0
+            
+        if check_valid_state: 
+
+            # making a choice and going to terminal state
+            if action != 2:
+                state_index = np.argwhere(self.trial_st[current_trial] == current_state)[0][0]
+                
+                # if a decision is made before the terminal states
+                if current_state < self.n_total_states - 5: # buffer states 
+                    
+                    if action == self.opt_act[current_trial, current_state]:
+                        reward = self.reward_magnitude
+                        next_state = self.n_total_states - 5 # transition into terminal states
+                    else:                                                       
+                        reward = self.punishment_magnitude
+                        next_state = self.n_total_states - 5 # transition into terminal states
+                
+                # when we are in the terminal states
+                elif self.n_total_states - 5 <= current_state < self.n_total_states - 1:
+                    reward = 0
+                    next_state = 0
+                
+                # when we reach the final terminal state we go back to the initial state
+                elif current_state == self.n_total_states:
+                    reward = 0
+                    next_state = 0
+ 
+            # hold action and moving along the state space
+            else:
+                state_index = np.argwhere(self.trial_st[current_trial] == current_state)[0][0] #current state index
+
+                if state_index < np.argmax(self.trial_st[current_trial]):
+                    next_state = self.trial_st[current_trial][state_index + 1]
+                else:
+                    next_state = 0
+                    reward = 0
+        else:
+            next_state = 0
+            reward = 0
+
+        
+        return next_state, reward
